66ad2a8bd906fedcf4c4ab05a7869f4c0354ce1b
zNadir <115375151+znadir@users.noreply.github.com>
2024-08-29 21:41:14 -0400

    refactor about page

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -13,8 +13,8 @@ export default function Footer() {
 
 	return (
 		<footer className='rounded-lg shadow bg-gray-900 mt-7'>
-			<div className='w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between'>
-				<span className='text-sm sm:text-center text-gray-400'>
+			<div className='w-full mx-auto max-w-screen-xl p-4 flex flex-col md:flex-row items-center justify-between'>
+				<span className='text-sm text-gray-400'>
 					© 2023-{currentYear} Nadir Zebiri. All Rights Reserved.
 				</span>
 				<ul className='flex flex-wrap items-center text-sm font-medium text-gray-400'>
